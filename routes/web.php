2d96a715878ebd49210c91eea78ea40a11b94240
Yordi Condori <yordice75@gmail.com>
2025-04-21 11:31:55 -0400

    proyecto base inicial

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DefinicionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ProductBacklogController;
+use App\Http\Controllers\HistoriausuarioController;
+use App\Http\Controllers\SprintbacklogController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::resource('/definiciones', DefinicionController::class)->names('definiciones');
+Route::resource('/productbacklog', ProductBacklogController::class)->names('productbackloges');
+Route::resource('/historiausuario', HistoriausuarioController::class)->names('historiausuarios');
+Route::resource('/sprintbacklog', SprintbacklogController::class)->names('sprintbackloges');
+
+
+//Para actualizar solo el estado
+Route::patch('/sprintbacklog/{id}/updateEstado/{tareaId}', [SprintbacklogController::class, 'updateEstado'])
+    ->name('sprintbackloges.updateEstado');
+
+
+require __DIR__.'/auth.php';
